c95772622c87752e972a21a5b64ac02ac19c4f3b
SergioRod21 <sergiorod1985@gmail.com>
2021-06-10 20:53:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import './App.css';
+
+const api = {
+  key: "0b8c2694ed487c7efc914e2f21cf82e7",
+  base: "https://api.openwheatermap.org/data/2.5/"
+
+}
+
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    let days = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input type="text" className='search-bar' placeholder="Search..."
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="wheater-box"></div>
+        <div className="wheater-box">
+          <div className="temperature">
+            15C
+          </div>
+          <div className="wheater">Sunny</div>
+        </div>
+      </main>
     </div>
   );
 }
